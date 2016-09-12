9ddfc6de09d8872634134a1d4a279ebff6484719
Sindhukumar <ssundar6@gmu.edu>
2016-09-12 14:55:09 -0400

    Second Commit

--- src/CarApp.java
+++ src/CarApp.java
@@ -11,7 +11,18 @@ public class CarApp {
 		System.out.println(car1.acceralate());
 		System.out.println("The "+car1.getColor()+" "+car1.getMake()+" is going "+car1.getSpeed()+" mph");
 		System.out.println(car1.stop());
-
+		System.out.println("\n------------\n");
+		car2.setColor("blue");
+		car2.setMake("jetta");
+		System.out.println(car1.start());
+		System.out.println(car2.start());
+		System.out.println(car1.acceralate());
+		System.out.println(car2.acceralate());
+		System.out.println(car2.stop("to have its emissions checked"));
+		System.out.println("The "+car2.getColor()+ " "+ car2.getMake()+" passed its emmision test");
+		System.out.println("The "+car1.getColor()+ " "+ car1.getMake()+" is going "+car1.getSpeed()+" mph");
+		System.out.println(car1.stop());
+		System.out.println("The "+car2.getColor()+ " "+ car2.getMake()+" passed "+"the "+car1.getColor()+ " "+ car1.getMake());
 	}
 
 }
